1fe2087ab64648b8e13884fa9ba701c8b8dc9cef
Demiffy <velnertomas78@gmail.com>
2024-12-14 22:10:08 +0100

    tailwind config test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    'text-red-600',
+    'text-9xl',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  important: true,
+};
